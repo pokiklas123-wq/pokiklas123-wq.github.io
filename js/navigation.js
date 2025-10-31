5ebfb703bf8063070959d89c55ac70bd746b8313
pokiklas123-wq <pokiklas123@gmail.com>
2025-10-31 02:38:42 +0100

    Implement manga sorting and state saving
    Signed-off-by: pokiklas123-wq <pokiklas123@gmail.com>

--- js/navigation.js
+++ js/navigation.js
@@ -6,11 +6,14 @@ class NavigationManager {
     setupEventListeners() {
         // العودة للصفحة الرئيسية
         document.getElementById('backToHome').addEventListener('click', () => {
+            app.saveState('homePage');
             ui.navigateToPage('homePage');
         });
 
         // العودة لصفحة تفاصيل المانجا
         document.getElementById('backToManga').addEventListener('click', () => {
+            const mangaId = mangaManager.getCurrentMangaId();
+            app.saveState('mangaDetailPage', mangaId);
             ui.navigateToPage('mangaDetailPage');
         });
 
@@ -25,8 +28,27 @@ class NavigationManager {
     }
 
     sortManga(sortType) {
-        // سيتم تطبيق الفرز لاحقاً
-        console.log('Sorting by:', sortType);
+        if (mangaManager.mangaData) {
+            let sortedManga = Object.keys(mangaManager.mangaData).map(key => {
+                return { id: key, ...mangaManager.mangaData[key] };
+            });
+
+            switch (sortType) {
+                case 'newest':
+                    sortedManga.sort((a, b) => (b.timestamp || 0) - (a.timestamp || 0));
+                    break;
+                case 'popular':
+                    sortedManga.sort((a, b) => (b.views || 0) - (a.views || 0));
+                    break;
+                case 'oldest':
+                    sortedManga.sort((a, b) => (a.timestamp || 0) - (b.timestamp || 0));
+                    break;
+                default:
+                    break;
+            }
+
+            mangaManager.displaySortedManga(sortedManga);
+        }
     }
 }
 
